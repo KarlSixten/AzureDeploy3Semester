62d386263850fa09cb9c3af1c10326f776485dcc
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-09-12 12:42:57 +0200

    Initial commit

---
+++ src/main/java/org/example/azuredeploy3semester/AzureDeploy3SemesterApplication.java
@@ -0,0 +1,13 @@
+package org.example.azuredeploy3semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDeploy3SemesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureDeploy3SemesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/azuredeploy3semester/AzureDeploy3SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.azuredeploy3semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDeploy3SemesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
